c3ec67160e8bd2eedd3de9b90e20207cf699fd69
Ko-PN <komyo2000@gmail.com>
2018-12-07 11:46:05 +0700

    Initial Commit

---
+++ src/main/java/koko/springframework/dailytaskrest/DailyTaskRestApplication.java
@@ -0,0 +1,12 @@
+package koko.springframework.dailytaskrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTaskRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTaskRestApplication.class, args);
+    }
+}
